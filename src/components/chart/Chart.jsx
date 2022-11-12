547f35d98091e4f468b71a767637d3f86b21fac5
Pugazhprince <89248877+Pugazhprince@users.noreply.github.com>
2022-11-12 23:30:17 +0530

    Created untill Single Page Day2

--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -1,8 +1,63 @@
 import React from "react";
 import "./chart.scss";
 
-const Chart = () => {
-  return <div className="chart">Chart</div>;
+//Chart Website "Recharts.org"
+import {
+  AreaChart,
+  Area,
+  XAxis,
+  CartesianGrid,
+  Tooltip,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { name: "January", Total: 1200 },
+  { name: "February", Total: 2100 },
+  { name: "March", Total: 900 },
+  { name: "April", Total: 1500 },
+  { name: "May", Total: 1800 },
+  { name: "June", Total: 1000 },
+  // { name: "July", Total: 1900 },
+  // { name: "August", Total: 2050 },
+  // { name: "September", Total: 900 },
+  // { name: "October", Total: 1880 },
+  // { name: "November", Total: 1840 },
+  // { name: "December", Total: 2500 },
+];
+
+const Chart = ({ aspect, title }) => {
+  return (
+    <div className="chart">
+      <div className="title">{title}</div>
+      <ResponsiveContainer width="100%" aspect={aspect}>
+        <AreaChart
+          width={730}
+          height={250}
+          data={data}
+          margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+        >
+          <defs>
+            <linearGradient id="total" x1="0" y1="0" x2="0" y2="1">
+              <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+              <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+            </linearGradient>
+          </defs>
+          <XAxis dataKey="name" stroke="grey" />
+
+          <CartesianGrid strokeDasharray="3 3" className="chartGrid" />
+          <Tooltip />
+          <Area
+            type="monotone"
+            dataKey="Total"
+            stroke="#8884d8"
+            fillOpacity={1}
+            fill="url(#total)"
+          />
+        </AreaChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 export default Chart;
